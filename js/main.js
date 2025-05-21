6c09387c358d6a5268eaca23228f3c413d1b5931
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-05-21 21:43:46 +0400

    added move for car

--- js/main.js
+++ js/main.js
@@ -3,12 +3,27 @@
 const treesListEl = document.querySelectorAll(".tree");
 const bg = document.querySelector(".bg");
 const startBtn = document.querySelector(".start-btn");
+const car = document.querySelector(".car");
 
 const treesList = [];
 const speed = 3;
+const step = 3;
 const bgHeight = bg.clientHeight;
 
 let isStop = false;
+
+const carInfo = {
+    currentX: car.offsetLeft,
+    currentY: car.offsetTop,
+    stopIdUp: null,
+    stopIdDown: null,
+    stopIdLeft: null,
+    stopIdRight: null,
+    isAnimatedUp: false,
+    isAnimatedDown: false,
+    isAnimatedLeft: false,
+    isAnimatedRight: false,
+};
 
 function moveBg(elementInfo) {
     return () => {
@@ -46,6 +61,33 @@ function stopGame() {
     }
 }
 
+function moveToTop() {
+    const y = carInfo.currentY - step;
+    carInfo.currentY = y;
+    car.style.top = y + "px";
+    carInfo.stopIdUp = requestAnimationFrame(moveToTop);
+}
+function moveToDown() {
+    const y = carInfo.currentY + step;
+    carInfo.currentY = y;
+    car.style.top = y + "px";
+    carInfo.stopIdDown = requestAnimationFrame(moveToDown);
+}
+function moveToLeft() {
+    const x = carInfo.currentX - step;
+    carInfo.currentX = x;
+    car.style.left = x + "px";
+    carInfo.stopIdLeft = requestAnimationFrame(moveToLeft);
+}
+function moveToRight() {
+    const x = carInfo.currentX + step;
+    carInfo.currentX = x;
+    car.style.left = x + "px";
+    carInfo.stopIdRight = requestAnimationFrame(moveToRight);
+}
+
+startBtn.addEventListener("click", stopGame);
+
 treesListEl.forEach((element) => {
     const tree = {
         el: element,
@@ -63,4 +105,65 @@ treesList.forEach((tree) => {
     requestAnimationFrame(moveBg(tree));
 });
 
-startBtn.addEventListener("click", stopGame);
+window.addEventListener("keydown", (evt) => {
+    const code = evt.code;
+
+    if (!carInfo.isAnimatedUp) {
+        if (code === "ArrowUp" || code === "KeyW") {
+            requestAnimationFrame(moveToTop);
+            carInfo.isAnimatedUp = true;
+        }
+    }
+    if (!carInfo.isAnimatedDown) {
+        if (code === "ArrowDown" || code === "KeyS") {
+            requestAnimationFrame(moveToDown);
+            carInfo.isAnimatedDown = true;
+        }
+    }
+
+    if (!carInfo.isAnimatedLeft) {
+        if (code === "ArrowLeft" || code === "KeyA") {
+            requestAnimationFrame(moveToLeft);
+            carInfo.isAnimatedLeft = true;
+        }
+    }
+
+    if (!carInfo.isAnimatedRight) {
+        if (code === "ArrowRight" || code === "KeyD") {
+            requestAnimationFrame(moveToRight);
+            carInfo.isAnimatedRight = true;
+        }
+    }
+});
+
+window.addEventListener("keyup", (evt) => {
+    const code = evt.code;
+    if (code === "ArrowUp" || code === "KeyW") {
+        cancelAnimationFrame(carInfo.stopIdUp);
+        carInfo.isAnimatedUp = false;
+    }
+
+    if (code === "ArrowDown" || code === "KeyS") {
+        cancelAnimationFrame(carInfo.stopIdDown);
+        carInfo.isAnimatedDown = false;
+    }
+
+    if (code === "ArrowLeft" || code === "KeyA") {
+        cancelAnimationFrame(carInfo.stopIdLeft);
+        carInfo.isAnimatedLeft = false;
+    }
+
+    if (code === "ArrowRight" || code === "KeyD") {
+        cancelAnimationFrame(carInfo.stopIdRight);
+        carInfo.isAnimatedRight = false;
+    }
+});
+
+// ArrowUp
+// KeyW
+// ArrowLeft
+// KeyA
+// ArrowRight
+// KeyD
+// ArrowDown
+// KeyS
